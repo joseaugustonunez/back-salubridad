f200bdd839d31f3f2ac638e3b41fcb46518ac814
joseaugustonunez <joseaugustonunezvicente@gmail.cmom>
2025-08-19 16:58:39 -0500

    mejorar tocke

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import 'dotenv/config'; 
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
